08f85e0fcb692cadb56067978f964b3fc6cc6c50
SaidMagomedow <71436061+SaidMagomedow@users.noreply.github.com>
2020-12-20 21:31:44 +0300

    Update settings.py

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -129,7 +129,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = [(
-    os.path.join(BASE_DIR, 'static_dev')
+    os.path.join(BASE_DIR, 'static')
 )]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
