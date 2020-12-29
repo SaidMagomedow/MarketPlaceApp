450a40804114f9f60999d5debdb00272bdaabf3e
SaidMagomedow <71436061+SaidMagomedow@users.noreply.github.com>
2020-12-29 13:44:39 +0300

    Update urls.py

--- shop/shop/urls.py
+++ shop/shop/urls.py
@@ -24,6 +24,5 @@ urlpatterns = [
     path('', include('ShopApp.urls')) #namespace='ShopApp:ShopApp'))
 ]
 # Если проект находится в стадии разработки , то все медиа файлы будут хранится в тех директория , которые мы указали
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
